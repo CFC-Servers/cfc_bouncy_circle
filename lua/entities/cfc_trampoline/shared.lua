024c5347176be9819d32dc71a8b97baec924b008
NullLikesVoid <82908498+NullLikesNothing@users.noreply.github.com>
2023-07-03 18:00:25 -0500

    Change contact

--- lua/entities/cfc_trampoline/shared.lua
+++ lua/entities/cfc_trampoline/shared.lua
@@ -2,7 +2,7 @@ ENT.Type = "anim"
 
 ENT.PrintName = "Trampoline"
 ENT.Author = "void pointer"
-ENT.Contact = "Don't"
+ENT.Contact = "cfc.gg/discord"
 ENT.Purpose = "Bouncy circle"
 ENT.Instructions = "Use with care."
 ENT.Spawnable = true
